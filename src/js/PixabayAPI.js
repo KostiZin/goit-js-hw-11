92a6abfec87eafb803c0ba545c9a20413029e386
KostiZin <pancostian@gmail.com>
2023-07-07 18:47:46 -0400

    task3

--- src/js/PixabayAPI.js
+++ src/js/PixabayAPI.js
@@ -1,25 +1,52 @@
+import axios from 'axios';
+
 export class PixabayAPI {
   #API_KEY = '38090711-3a880efc8e3f464ec3ef8c11b';
   #BASE_URL = 'https://pixabay.com/api/';
-  query = null;
+  query = '';
   page = 1;
 
-  fetchPhotos() {
-    return fetch(
+  fetchPhotos = () =>
+    axios.get(
       `${this.#BASE_URL}?key=${
         this.#API_KEY
-      }&q=nature&image_type=photo&orientation=horizontal&safesearch=true&page=${
+      }&q=&image_type=photo&orientation=horizontal&safesearch=true&page=${
         this.page
       }&per_page=40`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
+    );
 
-      return response.json();
-    });
-  }
+  fetchQuery = () =>
+    axios.get(
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+        this.query
+      }&image_type=photo&orientation=horizontal&safesearch=true&page=${
+        this.page
+      }&per_page=40`
+    );
+
+  // fetchPhotos() {
+  //   axios.get(
+  //     `${this.#BASE_URL}?key=${
+  //       this.#API_KEY
+  //     }&q=nature&image_type=photo&orientation=horizontal&safesearch=true&page=${
+  //       this.page
+  //     }&per_page=40`
+  //   );
+  // return fetch(
+  //   `${this.#BASE_URL}?key=${
+  //     this.#API_KEY
+  //   }&q=nature&image_type=photo&orientation=horizontal&safesearch=true&page=${
+  //     this.page
+  //   }&per_page=40`
+  // ).then(response => {
+  //   if (!response.ok) {
+  //     throw new Error(response.status);
+  //   }
+
+  //   return response.json();
+  // });
 }
+
 //   `${this.#BASE_URL}/search/photos?query=${this.query}&page=${
 //     this.page
 //   }&per_page=12&client_id=${this.#API_KEY}`
--- src/js/index.js
+++ src/js/index.js
@@ -5,35 +5,83 @@ import { createGalleryCard } from './createGalleryCard';
 
 const galleryEl = document.querySelector('.js-gallery');
 const searchFormEl = document.querySelector('.search-form');
-const loadMoreBtn = document.querySelector('.load-more');
+const loadMoreBtn = document.querySelector('.js-load-more');
 const inputEl = searchFormEl.firstElementChild;
+console.dir(inputEl);
 
 const api = new PixabayAPI();
+// let query = null;
 
 const options = {
-  totalItems: 0,
-  itemsPerPage: 12,
-  visiblePages: 5,
+  // totalItems: 0,
+  itemsPerPage: 40,
+  // visiblePages: 5,
   page: 1,
 };
 
-api.fetchPhotos().then(data => {
-  const markup = createGalleryCard(data);
-  console.log(data);
+// let isLabelActive = false;
 
-  galleryEl.innerHTML = markup;
-});
+api
+  .fetchPhotos()
+  .then(response => {
+    const markup = createGalleryCard(response.data);
+    // console.log(response.data);
+
+    galleryEl.innerHTML = markup;
+  })
+  .catch(console.warn);
 
 searchFormEl.addEventListener('submit', handleSearchForm);
+loadMoreBtn.addEventListener('click', handleLoadMoreBtn);
 
 function handleSearchForm(evt) {
   evt.preventDefault();
 
   const searchValue = inputEl.value.trim();
+  api.query = searchValue;
+  console.log(api.query);
+  console.log(searchValue);
 
   if (!searchValue) {
     return;
   }
-  console.dir(searchValue);
-  console.dir(evt.target.firstElementChild.value);
+
+  api.fetchQuery().then(response => {
+    api.page = 1;
+    const markup = createGalleryCard(response.data);
+
+    // console.log(response.data.total);
+
+    galleryEl.innerHTML = markup;
+
+    if (galleryEl.innerHTML === '') {
+      loadMoreBtn.classList.add('is-hidden');
+      // searchFormEl.reset();
+      return Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+    }
+    Notiflix.Notify.success(
+      `We found ${
+        response.data.total
+      } pictures for you under category "${searchValue.toUpperCase()}"`
+    );
+  });
+  // console.dir(searchValue);
+  // console.dir(evt.target.firstElementChild.value);
+  // console.log(query);
+}
+
+function handleLoadMoreBtn() {
+  api.page += 1;
+  console.log(api.page);
+
+  api.fetchQuery().then(response => {
+    console.log(response.data);
+    galleryEl.insertAdjacentHTML('beforeend', createGalleryCard(response.data));
+
+    if (api.page === response.data.total_pages) {
+      loadMoreBtn.classList.add('is-hidden');
+    }
+  });
 }
