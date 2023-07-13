07ef0704e80513ea10ab431838514eff2665bd43
KostiZin <pancostian@gmail.com>
2023-07-13 15:12:53 -0400

    Updated newFetch 2

--- src/js/newFetch.js
+++ src/js/newFetch.js
@@ -9,14 +9,66 @@ const inputEl = searchFormEl.firstElementChild;
 
 const api = new PixabayAPI();
 
+// CREATE MARKUP FOR THE FIRST DEFAULT PAGE //
 api
   .fetchPhotos()
   .then(data => {
     console.log(data);
 
     const markup = createGalleryCard(data);
-    // console.log(markup);
-
     galleryEl.innerHTML = markup;
   })
   .catch(error => console.warn(error));
+
+// CREATE EVENT "SEARCH" AND ITS FUNCTION //
+
+searchFormEl.addEventListener('submit', handleSearchForm);
+
+function handleSearchForm(evt) {
+  api.page = 1;
+  evt.preventDefault();
+
+  let searchValue = inputEl.value.trim();
+  api.query = searchValue;
+
+  if (!searchValue) {
+    return;
+  }
+
+  api
+    .fetchQuery()
+    .then(data => {
+      console.log(data);
+      // showLoadMoreBtn();
+      // const { data } = response;
+      const markup = createGalleryCard(data);
+      galleryEl.innerHTML = markup;
+
+      if (galleryEl.innerHTML === '') {
+        inputEl.value = '';
+        //   hideLoadMoreBtn();
+        return Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+      }
+      Notiflix.Notify.success(
+        `We found ${
+          data.totalHits
+        } pictures for you under category "${searchValue.toUpperCase()}"`
+      );
+
+      if (40 / data.hits.length > 1) {
+        hideLoadMoreBtn();
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+
+      //   galleryEl.innerHTML.refresh();
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+
+  inputEl.value = '';
+}
