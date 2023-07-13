61258195718bf396c2ea24d4c0a52eb87134370b
KostiZin <pancostian@gmail.com>
2023-07-13 16:03:52 -0400

    final without axis and async/await

--- src/js/PixabayNewFetch.js
+++ src/js/PixabayNewFetch.js
@@ -1,45 +1,4 @@
-// // FETCH
-// export class PixabayAPI {
-//   #API_KEY = '38090711-3a880efc8e3f464ec3ef8c11b';
-//   #BASE_URL = 'https://pixabay.com/api/';
-//   query = '';
-//   page = 1;
-
-//   fetchPhotos = () => {
-//     return fetch(
-//       `${this.#BASE_URL}?key=${
-//         this.#API_KEY
-//       }&q=&image_type=photo&orientation=horizontal&safesearch=true&page=${
-//         this.page
-//       }&per_page=40`
-//     )
-//       .then(response => {
-//         return response.json();
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   };
-
-//   fetchQuery = () => {
-//     return fetch(
-//       `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-//         this.query
-//       }&image_type=photo&orientation=horizontal&safesearch=true&page=${
-//         this.page
-//       }&per_page=40`
-//     )
-//       .then(response => {
-//         return response.json();
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   };
-// }
-
-// AXIOS ==============================================================
-import axios from 'axios';
+// FETCH
 export class PixabayAPI {
   #API_KEY = '38090711-3a880efc8e3f464ec3ef8c11b';
   #BASE_URL = 'https://pixabay.com/api/';
@@ -47,16 +6,15 @@ export class PixabayAPI {
   page = 1;
 
   fetchPhotos = () => {
-    return axios
-      .get(
-        `${this.#BASE_URL}?key=${
-          this.#API_KEY
-        }&q=&image_type=photo&orientation=horizontal&safesearch=true&page=${
-          this.page
-        }&per_page=40`
-      )
+    return fetch(
+      `${this.#BASE_URL}?key=${
+        this.#API_KEY
+      }&q=&image_type=photo&orientation=horizontal&safesearch=true&page=${
+        this.page
+      }&per_page=40`
+    )
       .then(response => {
-        return response.data;
+        return response.json();
       })
       .catch(error => {
         console.log(error);
@@ -64,19 +22,61 @@ export class PixabayAPI {
   };
 
   fetchQuery = () => {
-    return axios
-      .get(
-        `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-          this.query
-        }&image_type=photo&orientation=horizontal&safesearch=true&page=${
-          this.page
-        }&per_page=40`
-      )
+    return fetch(
+      `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+        this.query
+      }&image_type=photo&orientation=horizontal&safesearch=true&page=${
+        this.page
+      }&per_page=40`
+    )
       .then(response => {
-        return response.data;
+        return response.json();
       })
       .catch(error => {
         console.log(error);
       });
   };
 }
+
+// // AXIOS ==============================================================
+// import axios from 'axios';
+// export class PixabayAPI {
+//   #API_KEY = '38090711-3a880efc8e3f464ec3ef8c11b';
+//   #BASE_URL = 'https://pixabay.com/api/';
+//   query = '';
+//   page = 1;
+
+//   fetchPhotos = () => {
+//     return axios
+//       .get(
+//         `${this.#BASE_URL}?key=${
+//           this.#API_KEY
+//         }&q=&image_type=photo&orientation=horizontal&safesearch=true&page=${
+//           this.page
+//         }&per_page=40`
+//       )
+//       .then(response => {
+//         return response.data;
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   };
+
+//   fetchQuery = () => {
+//     return axios
+//       .get(
+//         `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
+//           this.query
+//         }&image_type=photo&orientation=horizontal&safesearch=true&page=${
+//           this.page
+//         }&per_page=40`
+//       )
+//       .then(response => {
+//         return response.data;
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   };
+// }
